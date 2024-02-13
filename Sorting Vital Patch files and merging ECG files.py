dff30a0339154fc3ced5c6155e2cf532eb3af5b1
Aboagye Nana Yaw <156020292+nanayawaboagye12@users.noreply.github.com>
2024-02-13 13:31:04 +0000

    Update Sorting Vital Patch files and merging ECG files.py

--- Sorting Vital Patch files and merging ECG files.py
+++ Sorting Vital Patch files and merging ECG files.py
@@ -33,9 +33,10 @@ xyzfiles=[file for file in sorted if file.endswith('xyz.csv')  ]
 
 
 ##A function to concatenate only ecg files of the vital patch over the whole recording period(NB:if you've got your files sorted in th the folder already)
-##If you don't have your files sorted already,you can just use he preceding code to sort the files before concatenating them.
+##If you don't have your files sorted already,you can just use the above code to sort the files before concatenating them.
 
 def getecgdf(path):
+    # path=''
     directories=os.listdir(path)
     ecgdf = []
     for file in directories:   
@@ -44,27 +45,49 @@ def getecgdf(path):
         
            if os.path.exists(ecgpath):
               ecg = pd.read_csv(ecgpath, header=None)
-              print(f'"Reading: {file}"')
+              print(f'"first file{file}"')
               # Get amplitude and timestamp columns
               amplitudes = ecg.iloc[:, 1::2].values.flatten()
               timestamps = ecg.iloc[:, 0::2].values.flatten()
-              print(f'"flattened: {file}"')
+              print('flattened')
               
               if ecg.isna().any().any()==False:
-                 print(f'"{file}has no missing value')
               # Create DataFrame for current file
                  df = pd.DataFrame({'Time': timestamps , 'Amplitude': amplitudes})
                  print('dataframe')
               # Append DataFrame to list
                  ecgdf.append(df)
 
-        ecgdf_concatenated = pd.concat(ecgdf, ignore_index=True)
+           ecgdf_concatenated = pd.concat(ecgdf, ignore_index=True)
+        
+   ##operations on the epoch
+    Time=ecgdf_concatenated['Time'].values
+    files_sorted_by_mtime = sorted(Time)
+    slicedtime=[(str(times))[:-3] for times in Time]
+    slicedtime=[(int(times)) for times in slicedtime]
+    timesinseconds=pd.to_datetime(slicedtime,unit='s')
+    ecgdf_concatenated['New Time']=timesinseconds
+    #sort time
+    ecgdf_concatenated.set_index('New Time', inplace=True)
+    # Sort DataFrame by index (epochs)
+    ecgdf_concatenated.sort_index(inplace=True)
+        
+    if len(pd.date_range(start=timesinseconds[0],end=timesinseconds[-1])) == 5:
+        print('The length of recording is 5 days,concatenated files for 5 days')
+
+    elif len(pd.date_range(start=timesinseconds[0],end=timesinseconds[-1])) == 6:
+        print('The length of recording is 6 days,concatenating files for 6 days')
+                         
+    elif len(pd.date_range(start=timesinseconds[0],end=timesinseconds[-1])) == 7:
+        print('The length of recording is 7 days,concatenating files for 7 days')    
+
+    elif len(pd.date_range(start=timesinseconds[0],end=timesinseconds[-1])) <= 4:
+        print(f'"The length of recording is {len(pd.date_range(start=timesinseconds[0],end=timesinseconds[-1]))},concatenating files for {len(pd.date_range(start=timesinseconds[0],end=timesinseconds[-1]))} days  "')
     
     return ecgdf_concatenated
 
 
 
-# Concatenate all DataFrames into a single DataFrame forvital pacth files and sort it brom the start date to end date
 
 # getting vitals dataframe
 def getvitaldf(path):
@@ -73,10 +96,10 @@ def getvitaldf(path):
     vitaldf = []
     for file in directories:   
         if file.endswith('_vitals.csv'):
-           ecgpath = os.path.join(path, file)
+           vitalpath = os.path.join(path, file)
         
-           if os.path.exists(ecgpath):
-              ecg = pd.read_csv(ecgpath, header=None)
+           if os.path.exists(vitalpath):
+              ecg = pd.read_csv(vitalpath, header=None)
               print(f'"first file{file}"')
               # Get amplitude and timestamp columns
               Posture = ecg.iloc[:,5].values.flatten()
@@ -89,25 +112,30 @@ def getvitaldf(path):
               print('flattened')
               
               if ecg.isna().any().any()==True:
-              # Create DataFrame for current file  
+              # Create DataFrame for current file
+                 # Time=ecg[0].values
+                 # files_sorted_by_mtime = sorted(Time)
+                 # slicedtime=[(str(times))[:-3] for times in Time]
+                 # slicedtime=[(int(times)) for times in slicedtime]
+                 # timesinseconds=pd.to_datetime(slicedtime,unit='s')   
                  df = pd.DataFrame({'Time': timestamps , 'Heart Rate':Heartrate ,'Respiration Rate':Respirationrate,'Temperature':Temperature,'Stepscount':Steps,'Posture':Posture,'R-R Interval':RRinterval})
                  print('dataframe')
               # Append DataFrame to list
                  vitaldf.append(df)
                  
-    ecgdf_concatenated = pd.concat(vitaldf, ignore_index=True)
+    vitaldf_concatenated = pd.concat(vitaldf, ignore_index=True)
     
     
-    Time=ecgdf_concatenated['Time'].values
+    Time=vitaldf_concatenated['Time'].values
     files_sorted_by_mtime = sorted(Time)
     slicedtime=[(str(times))[:-3] for times in Time]
     slicedtime=[(int(times)) for times in slicedtime]
     timesinseconds=pd.to_datetime(slicedtime,unit='s')
-    ecgdf_concatenated['New Time']=timesinseconds
+    vitaldf_concatenated['New Time']=timesinseconds
     #sort time
-    ecgdf_concatenated.set_index('New Time', inplace=True)
+    vitaldf_concatenated.set_index('New Time', inplace=True)
     # Sort DataFrame by index (epochs)
-    ecgdf_concatenated.sort_index(inplace=True)
+    vitaldf_concatenated.sort_index(inplace=True)
 
            
     if len(pd.date_range(start=timesinseconds[0],end=timesinseconds[-1])) == 5:
@@ -122,7 +150,74 @@ def getvitaldf(path):
     elif len(pd.date_range(start=timesinseconds[0],end=timesinseconds[-1])) <= 4:
          print(f'"The length of recording is {len(pd.date_range(start=timesinseconds[0],end=timesinseconds[-1]))},concatenating files for {len(pd.date_range(start=timesinseconds[0],end=timesinseconds[-1]))} days  "')
     
-    return ecgdf_concatenated
+    return vitaldf_concatenated
+
+
+
+
+
+#getxyzfiles
+
+def getxyzdf(path):
+    # path=''
+    directories=os.listdir(path)
+    xyzdf = []
+    for file in directories:   
+        if file.endswith('xyz.csv'):
+           xyzpath = os.path.join(path, file)
+        
+           if os.path.exists(xyzpath):
+              xyz = pd.read_csv(xyzpath, header=None)
+              print(f'"first file{file}"')
+              # Get amplitude and timestamp columns
+              z = xyz.iloc[:,3].values.flatten()
+              y = xyz.iloc[:,2].values.flatten()
+              
+              x = xyz.iloc[:, 1].values.flatten()
+              timestamps = xyz.iloc[:, 0].values.flatten()
+              print('flattened')
+              
+              if xyz.isna().any().any()==False:
+              # Create DataFrame for current file
+                 df = pd.DataFrame({'Time': timestamps , 'X-axis':x , 'Y-axis':y ,'Z-axis':z})
+                 print('dataframe')
+              # Append DataFrame to list
+                 xyzdf.append(df)
+
+                 # ecgdf_concatenated = pd.concat(xyzdf, ignore_index=True)
+    
+    
+    xyzdf_concatenated = pd.concat(xyzdf, ignore_index=True)
+    
+    
+    Time=xyzdf_concatenated['Time'].values
+    files_sorted_by_mtime = sorted(Time)
+    slicedtime=[(str(times))[:-3] for times in Time]
+    slicedtime=[(int(times)) for times in slicedtime]
+    timesinseconds=pd.to_datetime(slicedtime,unit='s')
+    xyzdf_concatenated['New Time']=timesinseconds
+    #sort time
+    xyzdf_concatenated.set_index('New Time', inplace=True)
+    # Sort DataFrame by index (epochs)
+    xyzdf_concatenated.sort_index(inplace=True)
+
+           
+    if len(pd.date_range(start=timesinseconds[0],end=timesinseconds[-1])) == 5:
+       print('The length of recording is 5 days,concatenated files for 5 days')
+
+    elif len(pd.date_range(start=timesinseconds[0],end=timesinseconds[-1])) == 6:
+         print('The length of recording is 6 days,concatenating files for 6 days')
+                     
+    elif len(pd.date_range(start=timesinseconds[0],end=timesinseconds[-1])) == 7:
+         print('The length of recording is 7 days,concatenating files for 7 days')    
+
+    elif len(pd.date_range(start=timesinseconds[0],end=timesinseconds[-1])) <= 4:
+         print(f'"The length of recording is {len(pd.date_range(start=timesinseconds[0],end=timesinseconds[-1]))},concatenating files for {len(pd.date_range(start=timesinseconds[0],end=timesinseconds[-1]))} days  "')
+    
+    
+     
+    return xyzdf_concatenated
+
 
 
 
